22b2c3267e16549a3ab2754e528cd405f9540fcb
R6i75i6r6irTh <165190852+R6i75i6r6irTh@users.noreply.github.com>
2024-03-27 17:45:08 +0100

    Update mail.js

--- src/js/mail.js
+++ src/js/mail.js
@@ -4,7 +4,7 @@ document.getElementById('tattooForm').addEventListener('submit', function(event)
     var formData = new FormData(this);
     
     // Send form data using Fetch API
-    fetch('https://script.google.com/macros/s/AKfycby0y-rRWkAQr9PEaT7Gffj_uBpCKC1aVztR-AwJRI7wh0IUEZvFDu--_FHYYU7eXr_EJQ/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzlmU1kjbzlF3QkJ-eaT4G63hYJNiipjAunS88yE8dGQgDywFx_kYImgrMn-BtJJcU/exec', {
         method: 'POST',
         body: formData
     })
